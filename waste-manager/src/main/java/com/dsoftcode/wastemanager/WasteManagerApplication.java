70084e8798f1d2eb133c4d2665a4f4db880d6a4d
ImDixan <dixanhm@gmail.com>
2024-04-13 05:58:58 -0400

    waste-manager module initial commit

---
+++ waste-manager/src/main/java/com/dsoftcode/wastemanager/WasteManagerApplication.java
@@ -0,0 +1,13 @@
+package com.dsoftcode.wastemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteManagerApplication.class, args);
+	}
+
+}
